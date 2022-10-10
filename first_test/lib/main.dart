2ae296e8123b5b3153c928513bff85c868f96b3e
Prox747 <carloproserpio1@gmail.com>
2022-10-10 21:44:03 +0200

    Codice per navigare tra due schermi

--- first_test/lib/main.dart
+++ first_test/lib/main.dart
@@ -10,6 +10,94 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+  @override
+  Widget build(BuildContext context) {
+    void navigateToHelpPage() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HelpPage()));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              const Text(
+                "Click the button for help",
+              ),
+              ElevatedButton(
+                onPressed: navigateToHelpPage,
+                child: const Icon(Icons.help),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HelpPage extends StatelessWidget {
+  const HelpPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Help Page"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const Text("Click the button to return to the Home Page"),
+              ElevatedButton(
+                onPressed: (() {
+                  Navigator.pop(context);
+                }),
+                child: const Text('Home'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
